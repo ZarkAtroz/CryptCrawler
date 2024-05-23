456d233437df8d744ea558abcc07cece96a208e4
Icaro Gabriel <icarogabi10@gmail.com>
2024-05-23 15:52:15 -0300

    Organização dos Pacotes

--- CryptCrawler/src/UI/Ui.java
+++ CryptCrawler/src/UI/Ui.java
@@ -1,3 +1,5 @@
+package UI;
+
 public class Ui {
 
     public static void main(String[] args) throws Exception {
--- CryptCrawler/src/game/Main.java
+++ CryptCrawler/src/game/Main.java
@@ -1,3 +1,5 @@
+package game;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
