fb0703ac1460b5ce31a8941ff60d78223e9a434d
John Chappell <john.claude.cameron@gmail.com>
2024-06-05 22:03:30 -0300

    Painéis fonte atualizado

--- CryptCrawler/src/Interface.java
+++ CryptCrawler/src/Interface.java
@@ -1,53 +1,50 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 // Para funcionar, vá para o IntelliJ, clique com o botão direito no arquivo .jar e clique em "add as a library" (ultima opção)
+import asciiPanel.AsciiFont;
 import asciiPanel.AsciiPanel;
 
 public class Interface extends JPanel {
 
-    private int x = 200; // posição inicial x do personagem
-    private int y = 200; // posição inicial y do personagem
-    private int moveSpeed;
     private AsciiPanel telaDeJogo;
     private AsciiPanel statusJogador;
     private AsciiPanel relatorioJogo;
     private AsciiPanel miniMapa;
 
-    private int telaDeJogoWidth = 80; // largura do painel ASCII (em caracteres)
-    private int telaDeJogoHeight = 24; // altura do painel ASCII (em caracteres)
+    private int telaDeJogoWidth = 48; // largura do painel ASCII (em caracteres)
+    private int telaDeJogoHeight = 32; // altura do painel ASCII (em caracteres)
     private int statusJogadorWidht = 80;
     private int statusJogadorHeight = 12;
     private int relatorioJogoWidht = 49;
-    private int relatorioJogoHeight = 20;
+    private int relatorioJogoHeight = 30;
     private int miniMapaWidht = 49;
-    private int miniMapaHeight = 16;
+    private int miniMapaHeight = 35;
 
-    private static final int CHAR_WIDTH = 9;
-    private static final int CHAR_HEIGHT = 16;
+    private static final int FONT_TALRYTH_SIZE = 15;
+    private static final int FONT_DRAKE_SIZE = 9;
 
     public Interface() {
         this.setFocusable(true);
 
-        telaDeJogo = new AsciiPanel();
-        telaDeJogo.setSize(telaDeJogoWidth * CHAR_WIDTH, telaDeJogoHeight * CHAR_HEIGHT);
-        telaDeJogo.setBounds(30, 30, telaDeJogoWidth * CHAR_WIDTH, telaDeJogoHeight * CHAR_HEIGHT);
+        telaDeJogo = new AsciiPanel(telaDeJogoWidth * FONT_TALRYTH_SIZE, telaDeJogoHeight * FONT_TALRYTH_SIZE, AsciiFont.TALRYTH_15_15);
+        telaDeJogo.setSize(telaDeJogoWidth * FONT_TALRYTH_SIZE, telaDeJogoHeight * FONT_TALRYTH_SIZE);
+        telaDeJogo.setBounds(30, 30, telaDeJogoWidth * FONT_TALRYTH_SIZE, telaDeJogoHeight * FONT_TALRYTH_SIZE);
         this.add(telaDeJogo);
 
-        statusJogador = new AsciiPanel();
-        statusJogador.setSize(statusJogadorWidht  * CHAR_WIDTH, statusJogadorHeight * CHAR_HEIGHT);
-        statusJogador.setBounds(30, 464, statusJogadorWidht * CHAR_WIDTH, statusJogadorHeight * CHAR_HEIGHT);
+        statusJogador = new AsciiPanel(statusJogadorWidht * FONT_DRAKE_SIZE, statusJogadorHeight * FONT_DRAKE_SIZE, AsciiFont.DRAKE_10x10);
+        statusJogador.setSize(statusJogadorWidht  * FONT_DRAKE_SIZE, statusJogadorHeight * FONT_DRAKE_SIZE);
+        statusJogador.setBounds(30, 545, statusJogadorWidht * FONT_DRAKE_SIZE, statusJogadorHeight * FONT_DRAKE_SIZE);
         this.add(statusJogador);
 
-        relatorioJogo = new AsciiPanel();
-        relatorioJogo.setSize(relatorioJogoWidht * CHAR_WIDTH, relatorioJogoHeight * CHAR_HEIGHT);
-        relatorioJogo.setBounds(790, 30, relatorioJogoWidht * CHAR_WIDTH, relatorioJogoHeight * CHAR_HEIGHT);
+        relatorioJogo = new AsciiPanel(relatorioJogoWidht * FONT_DRAKE_SIZE, relatorioJogoHeight * FONT_DRAKE_SIZE, AsciiFont.DRAKE_10x10);
+        relatorioJogo.setSize(relatorioJogoWidht * FONT_DRAKE_SIZE, relatorioJogoHeight * FONT_DRAKE_SIZE);
+        relatorioJogo.setBounds(790, 30, relatorioJogoWidht * FONT_DRAKE_SIZE, relatorioJogoHeight * FONT_DRAKE_SIZE);
         this.add(relatorioJogo);
 
-        miniMapa = new AsciiPanel();
-        miniMapa.setSize(miniMapaWidht * CHAR_WIDTH, miniMapaHeight * CHAR_HEIGHT);
-        miniMapa.setBounds(790, 390, miniMapaWidht * CHAR_WIDTH, miniMapaHeight * CHAR_HEIGHT);
+        miniMapa = new AsciiPanel(miniMapaWidht * FONT_DRAKE_SIZE, miniMapaHeight * FONT_DRAKE_SIZE, AsciiFont.DRAKE_10x10);
+        miniMapa.setSize(miniMapaWidht * FONT_DRAKE_SIZE, miniMapaHeight * FONT_DRAKE_SIZE);
+        miniMapa.setBounds(790, 337, miniMapaWidht * FONT_DRAKE_SIZE, miniMapaHeight * FONT_DRAKE_SIZE);
         this.add(miniMapa);
 
         updateAsciiPanel();
@@ -62,53 +59,35 @@ public class Interface extends JPanel {
 
         // Exemplo de uma sala vazia
         // largura: 80 | altura: 24 (em pixels)
-        char[][] matriz = new char[][]{
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','@','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-                {'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.',},
-        };
+        char[][] matriz = new char[telaDeJogoWidth][telaDeJogoHeight];
 
-//        telaDeJogo.write("Tela de jogo", 1, 1);
-        for(int i = 0; i < matriz.length; i++){
-            for(int j = 0; j < matriz[i].length; j++){
-                telaDeJogo.write(matriz[i][j], j, i);
+        for(int i = 0; i < 48; i++){
+            for(int j = 0; j < 32; j++){
+                matriz[i][j] = '.';
             }
-
         }
 
-        statusJogador.write("Status Jogador", 1, 1);
-        relatorioJogo.write("Relatorio Jogo", 1, 1);
-        miniMapa.write("Mini Mapa", 1, 1);
+        matriz[3][3] = (char) 9;
+
+        telaDeJogo.write("Tela de jogo", 1, 1);
+        for(int i = 0; i < 48; i++){
+            for(int j = 0; j < 32; j++){
+                telaDeJogo.write(matriz[i][j], i, j);
+            }
+        }
+
+
+        telaDeJogo.write(((char) 24) + "48", 45, 0);
+        telaDeJogo.write(((char) 27) + "32", 0, 31);
+
+        statusJogador.write("STATUS JOGADOR", 1, 1);
+        relatorioJogo.write("RELATORIO JOGO", 1, 1);
+        miniMapa.write("MINI MAPA", 1, 1);
 
         statusJogador.repaint();
         telaDeJogo.repaint();
         relatorioJogo.repaint();
         miniMapa.repaint();
-    }
-
-    public void setMoveSpeed(int moveSpeed){
-        this.moveSpeed = moveSpeed;
     }
 
     @Override
@@ -120,18 +99,18 @@ public class Interface extends JPanel {
         g.fillRect(0, 0, getWidth(), getHeight());
 
         g.setColor(Color.GRAY);
-        g.fillRect(20, 20, telaDeJogoWidth * CHAR_WIDTH + 20, telaDeJogoHeight * CHAR_HEIGHT + 20);
+        g.fillRect(20, 20, telaDeJogoWidth * 15 + 20, telaDeJogoHeight * 15 + 20);
 
-        g.fillRect(20, 444, statusJogadorWidht * CHAR_WIDTH + 20, statusJogadorHeight * CHAR_HEIGHT + 20);
+        g.fillRect(20, 535, statusJogadorWidht * FONT_DRAKE_SIZE + 20, statusJogadorHeight * FONT_DRAKE_SIZE + 20);
 
-        g.fillRect(780, 20, relatorioJogoWidht * CHAR_WIDTH + 20, relatorioJogoHeight * CHAR_HEIGHT + 20);
+        g.fillRect(780, 20, relatorioJogoWidht * FONT_DRAKE_SIZE + 20, relatorioJogoHeight * FONT_DRAKE_SIZE + 20);
 
-        g.fillRect(780, 380, miniMapaWidht * CHAR_WIDTH + 20, miniMapaHeight * CHAR_HEIGHT + 20);
+        g.fillRect(780, 327, miniMapaWidht * FONT_DRAKE_SIZE + 20, miniMapaHeight * FONT_DRAKE_SIZE + 20);
 
-        telaDeJogo.setBounds(30, 30, telaDeJogoWidth * CHAR_WIDTH, telaDeJogoHeight * CHAR_HEIGHT);
-        statusJogador.setBounds(30, 454, statusJogadorWidht * CHAR_WIDTH, statusJogadorHeight * CHAR_HEIGHT);
-        relatorioJogo.setBounds(790, 30, relatorioJogoWidht * CHAR_WIDTH, relatorioJogoHeight * CHAR_HEIGHT);
-        miniMapa.setBounds(790, 390, miniMapaWidht * CHAR_WIDTH, miniMapaHeight * CHAR_HEIGHT);
+        telaDeJogo.setBounds(30, 30, telaDeJogoWidth * FONT_TALRYTH_SIZE, telaDeJogoHeight * FONT_TALRYTH_SIZE);
+        statusJogador.setBounds(30, 545, statusJogadorWidht * FONT_DRAKE_SIZE, statusJogadorHeight * FONT_DRAKE_SIZE);
+        relatorioJogo.setBounds(790, 30, relatorioJogoWidht * FONT_DRAKE_SIZE, relatorioJogoHeight * FONT_DRAKE_SIZE);
+        miniMapa.setBounds(790, 337, miniMapaWidht * FONT_DRAKE_SIZE, miniMapaHeight * FONT_DRAKE_SIZE);
 
 
     }
--- CryptCrawler/src/Main.java
+++ CryptCrawler/src/Main.java
@@ -1,14 +1,18 @@
 import javax.swing.*;
 
-public class Main extends JFrame {
+public class Main extends JFrame{
 
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("Interface");
+        JFrame frame = new JFrame("Crypt Crawler");
+        ImageIcon img = new ImageIcon("src\\teste.jpg");
+        frame.setIconImage(img.getImage());
+
         Interface interfaceJogo = new Interface();
         frame.add(interfaceJogo);
         frame.setSize(1280, 720);
 
+        // Tela cheia
         // frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         // frame.setUndecorated(true);
 
@@ -16,6 +20,8 @@ public class Main extends JFrame {
         frame.setVisible(true);
         frame.setLayout(null);
 
+
     }
+
 
 }
